e48e53321ea24ca1081058c2c85819bb0154c1c1
dmytro_povarchuk <Dmytro_Povarchuk@epam.com>
2025-05-06 12:03:26 +0300

    Added changes

--- lab_08/main.tf
+++ lab_08/main.tf
@@ -22,10 +22,10 @@ terraform {
 # Configure Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+  # subscription_id = var.subscription_id
+  # tenant_id       = var.tenant_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
 }
 
 # Generate random string for storage account name
